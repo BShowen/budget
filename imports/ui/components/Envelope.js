a321d2bbc52a672f03483ac7517162847e343e32
Bradley Showen <BShowen@me.com>
2023-10-23 06:42:23 -0400

    Rename Category to Envelope

--- imports/ui/components/Envelope.js
+++ imports/ui/components/Envelope.js
@@ -6,8 +6,9 @@ import { decimal } from "../util/decimal";
 
 // Components
 import { Ledger } from "./Ledger";
+import { Progress } from "./Progress";
 
-export const Category = ({ name, startingBalance, ledgers, activeTab }) => {
+export const Envelope = ({ name, startingBalance, ledgers, activeTab }) => {
   const spent = ledgers.reduce((acc1, ledger) => {
     return (
       acc1 +
@@ -25,16 +26,21 @@ export const Category = ({ name, startingBalance, ledgers, activeTab }) => {
       : startingBalance;
 
   return (
-    <div className="bg-white rounded-lg shadow-md flex flex-col items-stretch px-2 pt-1">
-      <div className="flex flex-row justify-between p-1 pb-2">
+    // Envelope container
+    <div className="bg-white rounded-lg shadow-md flex flex-col items-stretch px-2 pt-1 gap-2">
+      {/* Envelope header */}
+      <div className="flex flex-row justify-between p-1 pb-2 relative h-8 border-2">
         <h1 className="font-bold">{cap(name)}</h1>
         <h2 className="font-semibold">{cap(activeTab)}</h2>
+        {/* <Progress percent={50} /> */}
       </div>
+      {/* Envelope body */}
       <div className="flex flex-col gap-2">
         {ledgers.map((ledger, i) => {
           return <Ledger key={i} {...ledger} activeTab={activeTab} />;
         })}
       </div>
+      {/* Envelope footer */}
       <div className="flex flex-row justify-between py-1">
         <p className="font-normal lg:hover:cursor-pointer">Add item</p>
         <h2 className="font-medium">{decimal(displayBalance)}</h2>
