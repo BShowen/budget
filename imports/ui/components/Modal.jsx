6dcf6ae3e26208d97fada45abdbd6783d06d374c
Bradley Showen <BShowen@me.com>
2023-10-25 16:39:55 -0400

    Refactor Modal- No longer apply default size styling to Modal

--- imports/ui/components/Modal.jsx
+++ imports/ui/components/Modal.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-
 export function Modal({ isOpen, onClose, children }) {
   useEffect(() => {
     if (isOpen) {
@@ -25,17 +24,9 @@ export function Modal({ isOpen, onClose, children }) {
         onClick={(e) => {
           e.stopPropagation();
         }}
-        className="bg-slate-100 w-full rounded-t-xl absolute bottom-0  overflow-scroll"
+        className="bg-slate-100 w-full rounded-t-xl absolute bottom-0 overflow-scroll"
       >
         {children}
-        <div className="flex flex-row justify-center items-center w-full p-2">
-          <h2
-            onClick={onClose}
-            className="inline-block text-center text-rose-500 font-bold text-lg"
-          >
-            Cancel
-          </h2>
-        </div>
       </div>
     </div>
   );
