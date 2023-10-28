8b338e747b2db3b8d53f1b69bcb7ad553ec173a8
Bradley Showen <BShowen@me.com>
2023-10-28 05:39:08 -0400

    Negative ledger balance is rendered $(number)

--- imports/ui/components/Ledger.js
+++ imports/ui/components/Ledger.js
@@ -58,7 +58,11 @@ export const Ledger = ({ _id, name, startingBalance, activeTab }) => {
     <div>
       <div className="flex flex-row justify-between items-center px-2 bg-slate-100 rounded-md py-1 lg:hover:cursor-pointer h-8 relative">
         <h2 className="font-semibold">{cap(name)}</h2>
-        <h2 className="font-normal">{decimal(displayBalance)}</h2>
+        <h2
+          className={`font-normal ${displayBalance < 0 ? "text-rose-600" : ""}`}
+        >
+          {decimal(displayBalance)}
+        </h2>
         <Progress percent={progress} />
       </div>
     </div>
--- imports/ui/util/decimal.js
+++ imports/ui/util/decimal.js
@@ -1,7 +1,11 @@
 // Simple function for parsing numbers into decimals and preceding them
 // with a dollar sign.
 export function decimal(number) {
-  return number !== null && number !== undefined
-    ? `$${Number.parseFloat(number).toFixed(2)}`
-    : "";
+  if (number === null || number === undefined) return "";
+
+  if (number < 0) {
+    return `$(${Number.parseFloat(Math.abs(number)).toFixed(2)})`;
+  } else {
+    return `$${Number.parseFloat(number).toFixed(2)}`;
+  }
 }
