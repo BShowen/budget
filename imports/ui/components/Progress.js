f76e5694a84aad09433cd8fd9fecffecacd52d87
Bradley Showen <BShowen@me.com>
2023-10-28 05:21:00 -0400

    Out of bounds ledger is red

--- imports/ui/components/Progress.js
+++ imports/ui/components/Progress.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export const Progress = ({ percent, overrideBgColor }) => {
-  const defaultBgColor = "bg-sky-500/30";
-  const bgColor = overrideBgColor ? overrideBgColor : defaultBgColor;
+export const Progress = (options) => {
+  const percent = options.percent < 0 ? 0 : options.percent;
+  const bgColor = percent <= 100 ? "bg-sky-500/30" : "bg-rose-500/30";
   return (
     <div className="w-full h-8 absolute left-0 right-0 rounded-md overflow-hidden">
       <div
