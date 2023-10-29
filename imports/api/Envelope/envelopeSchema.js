06192f3686a8fd67d29554bff9f266de7dadbff5
Bradley Showen <BShowen@me.com>
2023-10-29 08:11:40 -0400

    Add { isAllocated: Boolean } to Envelope schema

--- imports/api/Envelope/envelopeSchema.js
+++ imports/api/Envelope/envelopeSchema.js
@@ -3,6 +3,7 @@ import { ledgerSchema } from "../Ledger/ledgerSchema";
 
 export const envelopeSchema = new SimpleSchema({
   name: String,
+  isAllocated: Boolean, //Envelope can be allocated or unallocated
   startingBalance: Number,
   ledgers: { type: Array, optional: true },
   "ledgers.$": String, //ref ledgerSchema,
