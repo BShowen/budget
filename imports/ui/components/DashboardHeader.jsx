fe223be0723547fd43deaca875a6af0f1aa18a85
Bradley Showen <BShowen@me.com>
2023-11-02 06:50:15 -0400

    Header is fixed to top of screen

--- imports/ui/components/DashboardHeader.jsx
+++ imports/ui/components/DashboardHeader.jsx
@@ -14,15 +14,15 @@ import { RootContext } from "../pages/Splash";
 export function DashboardHeader({ setActiveTab, activeTab, date }) {
   const { setDate } = useContext(RootContext);
   return (
-    <div className="bg-sky-500 pb-3 shadow-md rounded-b-md">
-      <div className="w-full flex flex-row flex-nowrap items-center justify-between py-1">
+    <div className="bg-sky-500 shadow-md rounded-b-md px-2 h-28">
+      <div className="w-full flex flex-row flex-nowrap items-center justify-between">
         <div className="flex flex-row justify-center items-center">
           <BudgetDate date={date} />
           <TfiAngleDown className="text-white text-3xl lg:hover:cursor-pointer" />
         </div>
         <IoPersonCircleSharp className="text-5xl me-2 text-sky-700" />
       </div>
-      <div className="w-full px-1 py-4">
+      <div className="w-full px-1">
         <DashboardButtonGroup
           active={activeTab}
           setActiveTab={(activeTab) => setActiveTab(activeTab)}
