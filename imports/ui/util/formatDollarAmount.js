ef94f15b1cc5d3399a3c996da633552a63bc29b1
Bradley Showen <BShowen@me.com>
2023-11-05 16:16:17 -0500

    Improve formatDollar input handler

--- imports/ui/util/formatDollarAmount.js
+++ imports/ui/util/formatDollarAmount.js
@@ -1,56 +1,17 @@
-// Convert a string of numbers into a valid dollar amount.
-// For example
-// input    -->  output
-// "1"      --> "0.01"
-// "13"     --> "0.13"
-// "139"    --> "1.39"
-// "1390"   --> "13.90"
-// "13905"  --> "139.05"
-// "13955"  --> "139.55"
+// Convert a string of numbers into the format "0.00"
 
-// 0.01
 export function formatDollarAmount(value) {
-  value = value.toString();
-  if (
-    // If value is not a number
-    isNaN(value)
-  ) {
-    // Return the value minus the value that was just entered.
-    return value.slice(0, -1);
+  // convert empty strings to a zero
+  if (value.toString().trim().length == 0) {
+    value = "0";
   }
-
-  if (
-    // If no input provided
-    value.length == 0 ||
-    // If initial input is zero, ignore it.
-    value.toString() === "0"
-  ) {
-    // Ignore leading zeros
-    return "";
+  if (isNaN(value)) {
+    return value.slice(0, -1); // Return the input minus the bad value
   }
-
-  if (value.toString() === "0.0") return "";
-
-  // If value has "." remove it.
-  let workingValue = value.split(".").join("");
-  if (workingValue.length < 3) {
-    const pads = {
-      1: "0",
-      2: "00",
-    };
-
-    workingValue = pads[3 - workingValue.length] + workingValue;
-  }
-  // Split the value into an array of string ints
-  let splitWorkingValue = workingValue.split("");
-
-  if (splitWorkingValue[0] === "0" && splitWorkingValue.length > 3) {
-    // Remove leading zeros as the number grows.
-    splitWorkingValue.shift();
-  }
-
-  // Add the "." in the correct position
-  splitWorkingValue.splice(-2, 0, ".");
-  // Join and return
-  return splitWorkingValue.join("");
+  // Remove decimals
+  value = value.split(".").join("");
+  // Remove leading zeros
+  value = Number.parseInt(value);
+  // Return the floating point number
+  return Number.parseFloat(value / 100).toFixed(2);
 }
