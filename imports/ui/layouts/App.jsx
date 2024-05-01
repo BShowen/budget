d3e872871e1fb3b104d8cdf7955e0e8899f5d523
Bradley Showen <BShowen@me.com>
2024-05-01 19:11:40 -0400

    Add delay so loader doesnt flicker on screen during fast load times

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -24,18 +24,21 @@ export const App = () => {
   const { isLoading: isLoadingAppData, currentBudget } = useAppData({ date });
   const [isLoading, setIsLoading] = useState(true);
 
+  // When isLoadingAppData is false then set isLoading to false.
   useEffect(() => {
     if (isLoadingAppData) return;
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 900);
+    setIsLoading(false);
   }, [isLoadingAppData]);
 
-  function handleDateChange(date) {
+  // When the user selects a different month, set isLoading to true then wait
+  // 900ms before changing the date. This give the app enough time to animate
+  // out and also prevents the "currentBudget" from being set to undefined
+  // before the app unmounts. If that happens then an error occurs because
+  // currentBudget is expected to never be falsy.
+  function handleDateChange(newDate) {
+    if (newDate.getTime() == date.getTime()) return;
     setIsLoading(true);
-    setTimeout(() => {
-      setDate(date);
-    }, 900);
+    setTimeout(() => setDate(newDate), 900);
   }
 
   return (
