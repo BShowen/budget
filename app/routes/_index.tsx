d080b382cbdb889f31151b493c603513ed9e3a1d
Bradley Showen <BShowen@me.com>
2025-05-26 01:42:11 -0400

    Init landing page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router";
 export default function Route() {
-  return <p>Landing page</p>;
+  return (
+    <main className="max-w-7xl w-full mx-auto">
+      <div className="flex justify-center pt-10">
+        <h1>Welcome to Dough Tracker</h1>
+      </div>
+      <div className="flex flex-col items-center">
+        <p>
+          Please{" "}
+          <Link className="underline text-accent" to={"/sign-in"}>
+            sign in
+          </Link>{" "}
+          or{" "}
+          <Link className="underline text-accent" to={"/sign-up"}>
+            sign up
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
 }
